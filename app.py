3141c8896a532ac0b96f7c123cfe0dc826c7cc34
LAYHAK <layhak.garu@gmail.com>
2024-05-10 09:43:29 +0700

    Can Crud and append with static std_list

--- app.py
+++ app.py
@@ -1,11 +1,206 @@
-from flask import Flask
+import sqlite3
 
+from flask import Flask, render_template, request, redirect
+
+from user_list import std_list
+
+
+def get_db_connection():
+    conn = sqlite3.connect('db/db.sqlite3')
+    conn.row_factory = sqlite3.Row  # This allows you to access columns by names
+    return conn
+
+
+# student = cur.execute("SELECT * FROM student")
+#
+# for row in student:
+#     std_list.append({
+#         'id': len(std_list) + 1,
+#         'name': row[1],
+#         'gender': row[2],
+#         'email': row[3],
+#         'phone': row[4],
+#         'address': row[5]
+#     })
+#
 app = Flask(__name__)
+
+conn = get_db_connection()
+students = conn.execute("SELECT * FROM student").fetchall()
+for row in students:
+    std_list.append({
+        'id': row[0] + len(std_list),
+        'name': row[1],
+        'gender': row[2],
+        'phone': row[3],
+        'email': row[4],
+        'address': row[5]
+    })
+conn.close()
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return '<h1 style="color:red;">Hello World</h1>'
+def index():  # put application's code here
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/service')
+def service():
+    return render_template('service.html')
+
+
+@app.route('/why')
+def why():
+    return render_template('why.html')
+
+
+@app.route('/admin/dashboard')
+def dashboard():
+    module = 'dashboard'
+    return render_template('admin/index.html', module=module)
+
+
+@app.route('/admin/table')
+def table():
+    module = 'table'
+    return render_template('admin/user-table.html', module=module, data=std_list)
+
+
+@app.route('/admin/table/add')
+def add():
+    return render_template('admin/add-user-form.html')
+
+
+@app.post('/admin/table/create')
+def create():
+    name = request.form['name']
+    gender = request.form['gender']
+    email = request.form['email']
+    phone = request.form['phone']
+    address = request.form['address']
+    conn = get_db_connection()
+    conn.execute("INSERT INTO student (name, gender, email, phone, address) VALUES (?, ?, ?, ?, ?)",
+                 (name, gender, email, phone, address))
+    std_list.append({
+        'id': len(std_list) + 1,
+        'name': name,
+        'gender': gender,
+        'phone': phone,
+        'email': email,
+        'address': address
+    })
+    conn.commit()
+    conn.close()
+    return redirect('/admin/table')
+
+
+@app.route('/admin/edit')
+def edit():
+    module = 'table'
+    id = request.args.get('id', default=1, type=int)
+    current_user = filter(lambda x: x['id'] == id, std_list)
+    current_user = list(current_user)
+    name = current_user[0]['name']
+
+    return render_template('admin/update-user-form.html', module=module, user=current_user[0])
+
+
+@app.post('/admin/update')
+def update():
+    # return 'update:' + request.form['id']
+    id = int(request.form['id'])
+    name = request.form['name']
+    gender = request.form['gender']
+    email = request.form['email']
+    phone = request.form['phone']
+    address = request.form['address']
+
+    # Check if the ID is less than 10, if so, redirect without updating
+    if id < 10:
+        for user in std_list:
+            if user['id'] == id:
+                user.update({
+                    'name': name,
+                    'gender': gender,
+                    'email': email,
+                    'phone': phone,
+                    'address': address
+                })
+                break
+
+        return redirect('/admin/table')  # Redirect back or to another appropriate page
+
+    conn = get_db_connection()
+    conn.execute("UPDATE student SET name=?, gender=?, email=?, phone=?, address=? WHERE id=?",
+                 (name, gender, email, phone, address, id - len(std_list) + 1))
+    conn.commit()
+
+    # Update the std_list if it's still being used
+    for user in std_list:
+        if user['id'] == id:
+            user.update({
+                'name': name,
+                'gender': gender,
+                'email': email,
+                'phone': phone,
+                'address': address
+            })
+            break
+
+    conn.close()
+    return redirect('/admin/table')
+
+
+@app.route('/admin/show')
+def show():
+    module = 'table'
+    current_user = request.args.get('name', default='name', type=str)
+    user_dict = filter(lambda x: x['name'] == current_user, std_list)
+    user_dict = list(user_dict)
+    return render_template('admin/show-user-cards.html', module=module, user=user_dict[0])
+
+
+@app.route('/admin/confirm-delete')
+def confirm_delete():
+    module = 'table'
+    name = request.args.get('name', default='name', type=str)
+    user_dict = filter(lambda x: x['name'] == name, std_list)
+    user_dict = list(user_dict)
+    return render_template('admin/delete-user-cards.html', module=module, user=user_dict[0])
+
+
+@app.post('/admin/delete')
+def delete():
+    id = int(request.form['id'])
+    if id < 10:
+        for user in std_list:
+            if user['id'] == id:
+                std_list.remove(user)
+                break
+
+        return redirect('/admin/table')
+
+    conn = get_db_connection()
+    conn.execute("DELETE FROM student WHERE id=?", (id - len(std_list),))
+    conn.commit()
+    conn.close()
+
+    for user in std_list:
+        if user['id'] == id:
+            std_list.remove(user)
+            break
+
+    return redirect('/admin/table')
+
+
+@app.errorhandler(404)
+def error_404(e):
+    return render_template('admin/404.html')
 
 
 if __name__ == '__main__':
---
+++ user_list.py
@@ -0,0 +1,82 @@
+std_list = [
+    {
+        'id': 1,
+        'name': 'Joeanna Bragg',
+        'gender': 'male',
+        'phone': '947306912',
+        'email': 'lakitha_goodwillhick@buy.bg',
+        'address': 'Differential Road 4168, Baldwinville, Luxembourg, 064366'
+    },
+    {
+        'id': 2,
+        'name': 'Chico Ransome',
+        'gender': 'female',
+        'phone': '1234567890',
+        'email': 'laquinta_urenaiolt@meals.zj',
+        'address': 'Anaheim Street 2588, Dornbirn, French Guiana, 443412'
+    },
+    {
+        'id': 3,
+        'name': 'Symantha Greig',
+        'gender': 'male',
+        'phone': '1234567890',
+        'email': 'demitris_deforgea@kirk.gdc',
+        'address': 'Cream St 5781, Trosa, Ireland, 033480'
+    },
+    {
+        'id': 4,
+        'name': 'Laguisha Giordano',
+        'gender': 'female',
+        'phone': '1234567890',
+        'email': 'carnesha_lucio1pz@registrar.mw',
+        'address': 'Stud Road 3637, Rachel, Laos, 756571'
+    },
+    {
+        'id': 5,
+        'name': 'Francelia Lipe',
+        'gender': 'male',
+        'phone': '1234567890',
+        'email': 'nikkole_coronadoogmh@monitoring.qs',
+        'address': 'Daughter St 2485, Laura, Palau, 210261'
+    },
+    {
+        'id': 6,
+        'name': 'John Smith',
+        'gender': 'female',
+        'phone': '1234567890',
+        'email': 'bo_propereo@cells.phs',
+        'address': 'Admitted Road 9525, Bandarawela, Venezuela, 308606'
+    },
+    {
+        'id': 7,
+        'name': 'Shawnae Pardee',
+        'gender': 'male',
+        'phone': '1234567890',
+        'email': 'baudelia_ratnerhc5v@famous.qco',
+        'address': 'Ticket Street 3822, Marblemount, British Virgin Islands, 754123'
+    },
+    {
+        'id': 8,
+        'name': 'Achary Simmers',
+        'gender': 'female',
+        'phone': '1234567890',
+        'email': 'ruven_mcdougallxcjl@mounted.ix',
+        'address': 'tora_deloneywdkj@strengthen.dwu'
+    },
+    {
+        'id': 9,
+        'name': 'Nely Peckham',
+        'gender': 'male',
+        'phone': '1234567890',
+        'email': 'shante_hickersonqe@communication.qi',
+        'address': 'Cleared St 2850, Orange, France, 103927'
+    },
+    {
+        'id': 10,
+        'name': 'Angele Kahler',
+        'gender': 'female',
+        'phone': '1234567890',
+        'email': 'rayleen_streetmanf5qc@salmon.cxm',
+        'address': 'Beneath Road 7901, Crowsnest Pass, Guatemala, 377801'
+    },
+]
